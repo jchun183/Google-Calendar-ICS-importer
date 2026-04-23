e541eacc3e53d1f323a90062efb1e4ae498b1bde
jchun183 <jonathanchunemail@gmail.com>
2026-04-23 14:54:16 -0700

    icons

--- setup.bat
+++ setup.bat
@@ -1,12 +1,15 @@
 @echo off
-echo Setting up "Import to Google Calendar" right-click menu for current user...
+echo Setting up "Import to Google Calendar" with icon...
 
 set "BAT_PATH=%~dp0import_to_calendar.bat"
+set "ICON_PATH=%~dp0calendar.ico"
 
 reg add "HKCU\Software\Classes\SystemFileAssociations\.ics\shell\ImportToGoogleCalendar" /ve /d "Import to Google Calendar" /f
+
+reg add "HKCU\Software\Classes\SystemFileAssociations\.ics\shell\ImportToGoogleCalendar" /v Icon /d "%ICON_PATH%" /f
+
 reg add "HKCU\Software\Classes\SystemFileAssociations\.ics\shell\ImportToGoogleCalendar\command" /ve /d "\"%BAT_PATH%\" \"%%1\"" /f
 
 echo.
-echo Setup complete.
-echo If you do not see it immediately, close and reopen File Explorer.
+echo Setup complete!
 pause
